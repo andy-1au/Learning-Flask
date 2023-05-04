4bb8b9607b78f5794c519a41c7c4b406649a32bc
andy-1au <andyolau888@gmail.com>
2023-05-04 19:48:30 -0400

    added about page

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/") # root page of the website
-def hello(): 
-    return "<h1>Hello World!</h1>"
-
-@app.route("/home") # test home page
+@app.route("/") # test home page
 def home(): 
     return "<h1>This is a home page!</h1>"
 
+@app.route("/about")
+def about():
+    return "<h1> About Page</h1>"
 
 
 
