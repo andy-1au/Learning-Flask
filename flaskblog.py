16cf0632682f0306cf973cccb45435159b87e2f4
andy-1au <andyolau888@gmail.com>
2023-05-04 20:17:29 -0400

    set home() to return the list of posts

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
 
+posts = [
+    {
+        'author': 'Andy Lau',
+        'title': 'Blog Post 1',
+        'content': 'This is my first blog post.',
+        'date_posted': 'May 04, 2023',
+    },
+    {
+        'author': 'Dennis Lam',
+        'title': 'Blog Post 2',
+        'content': 'Hi, my name is Dennis Lam.',
+        'date_posted': 'May 04, 2023',
+    }
+]
+
 
 app = Flask(__name__)
 
 @app.route('/') # default
 @app.route('/home') # also home, two routes are handled by the same function below
 def home(): 
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
