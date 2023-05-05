e6345259a8c94d77d7012b4362e91cc65e2cafbd
andy-1au <andyolau888@gmail.com>
2023-05-04 22:16:32 -0400

    added flash message template and logic if the form validates properly

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -29,9 +29,12 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register(): 
     form = RegistrationForm() # create an instance of the class
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success') # success is a bootstrap class to style this alert
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
