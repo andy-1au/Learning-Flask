ba6894f9a47cee503cad4a0ab2da2708ac5d7ac3
andy-1au <andyolau888@gmail.com>
2023-05-04 20:00:24 -0400

    return render_template instead to read in templates for html

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
-@app.route("/") # default
-@app.route("/home") # also home, two routes are handled by the same function below
+@app.route('/') # default
+@app.route('/home') # also home, two routes are handled by the same function below
 def home(): 
-    return "<h1>This is a home page!</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1> About Page</h1>"
-
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
