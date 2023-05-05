8c09cdbbc7065d1eeeec8322cc0a4b577818ff65
andy-1au <andyolau888@gmail.com>
2023-05-04 23:09:48 -0400

    added flash message and test case for login attempt

--- flaskblog.py
+++ flaskblog.py
@@ -35,12 +35,20 @@ def register():
     if form.validate_on_submit(): # if .validate_on_submit() returns true, call the flash() message function 
         # flash message is a one time thing, so if the page is refreshed, then it will disappear
         flash(f'Account created for {form.username.data}!', 'success') # success is a bootstrap class to style this alert
+        # .data is the just data submitted in the username section of the form
         return redirect(url_for('home')) # redirect to the home url 
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login(): 
     form = LoginForm() # create an instance of the class
+    if form.validate_on_submit(): # same thing here
+        #temp test data before database
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':     
+            flash('You have successfully logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
