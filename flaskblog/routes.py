03d79281ff659b2cd61d1cc8a824b780d359c346
andy-1au <andyolau888@gmail.com>
2023-05-06 16:11:00 -0400

    added pagination and starting to work on user posts page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -9,17 +9,23 @@ from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, Post
 from flaskblog.models import MyUser, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-
+# HOME ROUTE -----------------------------------------------------------------
 @app.route('/') # default
 @app.route('/home') # also home, two routes are handled by the same function below
 def home(): 
-    posts = Post.query.order_by(Post.date_posted.desc()).all() # Query all posts from db in descending order by date_posted
+    page = request.args.get('page', 1, type=int) # get the page number from the url, default to 1, and type is int
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5) # 5 posts per page, ordered by most recent data
+    # posts = Post.query.order_by(Post.date_posted.desc()).all() # Query all posts from db in descending order by date_posted
     return render_template('home.html', posts=posts)
+# ----------------------------------------------------------------------------
 
+# ABOUT ROUTE ----------------------------------------------------------------
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+# ----------------------------------------------------------------------------
 
+# REGISTER ROUTE -------------------------------------------------------------
 @app.route("/register", methods=['GET', 'POST'])
 def register(): 
     # gets redirected to homepage is user is logged in
@@ -39,7 +45,9 @@ def register():
         # .data is the just data submitted in the username section of the form
         return redirect(url_for('login')) # redirect to the home url 
     return render_template('register.html', title='Register', form=form)
+# ----------------------------------------------------------------------------
 
+# LOGIN ROUTE ----------------------------------------------------------------
 @app.route("/login", methods=['GET', 'POST'])
 def login(): 
     # gets redirected to homepage is user is logged in
@@ -60,13 +68,17 @@ def login():
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+# ----------------------------------------------------------------------------
 
+# LOGOUT ROUTE ---------------------------------------------------------------
 @app.route("/logout")
 def logout():
     # don't need to pass in the user, and clears remember me cookie if it was selected
     logout_user()
     return redirect(url_for('home'))
+# ----------------------------------------------------------------------------
 
+# SAVE PICTURE FUNCTION ------------------------------------------------------
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8) # creating a random hex for the name of the picture file
     _, f_ext = os.path.splitext(form_picture.filename) # split the file name and the extension
@@ -79,7 +91,9 @@ def save_picture(form_picture):
     i.save(picture_path) 
     
     return picture_fn
+# ----------------------------------------------------------------------------
 
+# ACCOUNT ROUTE --------------------------------------------------------------
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
@@ -101,7 +115,9 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file) # set the image file to the current user's image file
     return render_template('account.html', title='Account', 
                            image_file=image_file, form=form)
+# ----------------------------------------------------------------------------
 
+# NEW POST ROUTE -------------------------------------------------------------
 @app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
@@ -116,12 +132,16 @@ def new_post():
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post',
                            form=form, legend="New Post")
+# ----------------------------------------------------------------------------
 
+# POST/ID ROUTE --------------------------------------------------------------
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id) # if the post doesn't exist, return 404, otherwise return the post
     return render_template('post.html', title=post.title, post=post)
+# ----------------------------------------------------------------------------
 
+# UPDATE POST ROUTE ----------------------------------------------------------
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
@@ -134,7 +154,8 @@ def update_post(post_id):
     if form.validate_on_submit(): 
         post.title = form.title.data
         post.content = form.content.data
-        post.date_posted = datetime.utcnow() # update the date posted to the current time so posts are can be sorted by most recent
+        # update the date posted to the current time so posts are can be sorted by most recent
+        post.date_posted = datetime.utcnow() 
         db.session.commit() # no need to add since it's already in the db, just updating
         flash('Your post has been updated!', 'success')
         return redirect(url_for('post', post_id=post.id)) # redirect to the post page
@@ -144,7 +165,9 @@ def update_post(post_id):
         form.content.data = post.content
     return render_template('create_post.html', title='Update Post', 
                            form=form, legend="Update Post")
-    
+# ----------------------------------------------------------------------------
+
+# DELETE POST ROUTE ---------------------------------------------------------- 
 @app.route("/post/<int:post_id>/delete", methods=['POST']) # only allow post requests because we don't want users to be able to go to the url and delete posts
 @login_required
 def delete_post(post_id):
@@ -156,4 +179,16 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home')) # redirect to the home page
+# ----------------------------------------------------------------------------
+
+# USER POSTS ROUTE -----------------------------------------------------------
+@app.route('/user/<str:username>') 
+def user_posts(username): 
+    page = request.args.get('page', 1, type=int)
+    user = MyUser.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date.posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
+# ----------------------------------------------------------------------------
 
