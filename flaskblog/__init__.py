197960a82c16bb866d88d9958c9beb8834548ded
andy-1au <andyolau888@gmail.com>
2023-05-05 20:56:16 -0400

    added bcrypt, for hashing password, and added create user in register route

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 from datetime import datetime
 
@@ -7,5 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c30cdec540796525fe21a63113d6b863' # Protect against attacks, modifying cookies, cross-site attacks
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # /// is a relative path to the project directory 
 db = SQLAlchemy(app) # create sqlalchemy database instance, classes/models are the database structures for SQLAlchemy
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from flaskblog import app
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import MyUser, Post
+
 
 posts = [
     {
@@ -30,11 +31,16 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register(): 
     form = RegistrationForm() # create an instance of the class
-    if form.validate_on_submit(): # if .validate_on_submit() returns true, call the flash() message function 
+    if form.validate_on_submit(): # if everything in the form is correct
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8') # hash the password in the form on validate, and decode it to a string
+        user = MyUser(username=form.username.data, email=form.email.data, password=hashed_password) # generate new user using the info passed into form
+        db.session.add(user)
+        db.session.commit()
+
         # flash message is a one time thing, so if the page is refreshed, then it will disappear
-        flash(f'Account created for {form.username.data}!', 'success') # success is a bootstrap class to style this alert
+        flash(f'Your account has been created! You are now able to log in.', 'success') # success is a bootstrap class to style this alert
         # .data is the just data submitted in the username section of the form
-        return redirect(url_for('home')) # redirect to the home url 
+        return redirect(url_for('login')) # redirect to the home url 
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
