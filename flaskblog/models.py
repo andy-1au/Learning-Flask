f303252706d9405fbb22303b33432dfe89eda570
andy-1au <andyolau888@gmail.com>
2023-05-07 12:01:02 -0400

    changed app to current_app

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 # from itsdangerous import URLSafeTimedSerializer as Serializer
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flaskblog import db, login_manager, app
+from flask import current_app
+from flaskblog import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -22,12 +23,12 @@ class MyUser(db.Model, UserMixin):
     # Function for printing out the user object
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec) # create a serializer object
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec) # create a serializer object
         return s.dumps({'user_id': self.id}).decode('utf-8') # return a token that expires in 30 mins, and the user id
     
     @staticmethod # don't expect self as an argument
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token)['user_id'] # get the user id from the token
         except:
