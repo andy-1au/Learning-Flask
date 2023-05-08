2a4591d375c01e373cf595c2df1e583ba4fe147d
andy-1au <andyolau888@gmail.com>
2023-05-07 23:16:27 -0400

    removed comments on environ vars

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,11 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') # the secret key is used to protect against modifying cookies and cross-site request forgery attacks
     
-    # POSTGRES_USER = os.environ.get("POSTGRES_USER")
-    # POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-    # POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
-    # POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
-    # POSTGRES_DB = os.environ.get("POSTGRES_DB")
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
 
     MAIL_SERVER = 'smtp.googlemail.com'  # smtp server for gmail
